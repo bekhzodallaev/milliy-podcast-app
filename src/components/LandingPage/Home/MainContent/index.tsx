3af5cb0975735c67898bd87ef0f81807424fd920
bekhzod <icebreaker0210@mail.ru>
2023-10-18 22:18:00 +0200

    Make style related chnages

--- src/components/LandingPage/Home/MainContent/index.tsx
+++ src/components/LandingPage/Home/MainContent/index.tsx
@@ -13,18 +13,22 @@ const MainContent: React.FC<YourComponentProps> = (props) => {
       <div className="navbar-content">
         <Navbar />
       </div>
-      <div className="left-side">
-        <div className="main-text">Discover the world of podcasts with us</div>
-        <div className="content-text">
-          Experience a new way to enjoy podcasts with our innovative app.
-          Browse, and share your favorite shows anytime, anywhere.
+      <div className="main-container-1">
+        <div className="left-side">
+          <div className="main-text">
+            Discover the world of podcasts with us
+          </div>
+          <div className="content-text">
+            Experience a new way to enjoy podcasts with our innovative app.
+            Browse, and share your favorite shows anytime, anywhere.
+          </div>
+          <div className="main-buttons">
+            <button className="browse-episodes">Browse Episodes</button>
+            <button className="become-podcaster">Become a podcaster</button>
+          </div>
         </div>
-        <div className="main-buttons">
-          <button className="browse-episodes">Browse Episodes</button>
-          <button className="become-podcaster">Become a podcaster</button>
-        </div>
+        <div className="right-side"></div>
       </div>
-      <div className="right-side"></div>
     </div>
   );
 };
