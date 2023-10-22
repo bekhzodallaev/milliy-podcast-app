81e86b9926df55872b8b1707b728343799e508d3
Botirjon Shokirov <botirjon@radcode.co>
2023-10-22 12:14:24 +0200

    Migrated Main content

--- src/components/LandingPage/Home/MainContent/index.tsx
+++ src/components/LandingPage/Home/MainContent/index.tsx
@@ -1,29 +1,31 @@
 import React from "react";
-import "./MainContent.css";
+import classes from "./MainContent.module.css";
 
 import Navbar from "../Navbar/Navbar";
 
 const MainContent: React.FC = (props) => {
   return (
-    <div className="main-content">
-      <div className="navbar-content">
+    <div className={classes.main_content}>
+      <div className={classes.navbar_content}>
         <Navbar />
       </div>
-      <div className="main-container-1">
-        <div className="left-side">
-          <div className="main-text">
+      <div className={classes.main_container_1}>
+        <div className={classes.left_side}>
+          <div className={classes.main_text}>
             Discover the world of podcasts with us
           </div>
-          <div className="content-text">
+          <div className={classes.content_text}>
             Experience a new way to enjoy podcasts with our innovative app.
             Browse, and share your favorite shows anytime, anywhere.
           </div>
-          <div className="main-buttons">
-            <button className="browse-episodes">Browse Episodes</button>
-            <button className="become-podcaster">Become a podcaster</button>
+          <div className={classes.main_buttons}>
+            <button className={classes.browse_episodes}>Browse Episodes</button>
+            <button className={classes.become_podcaster}>
+              Become a podcaster
+            </button>
           </div>
         </div>
-        <div className="right-side"></div>
+        <div className={classes.right_side}></div>
       </div>
     </div>
   );
