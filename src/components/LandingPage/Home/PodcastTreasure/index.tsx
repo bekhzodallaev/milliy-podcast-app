f17efcd2f73a44c368297cf01f7a55af0560dd0b
Botirjon Shokirov <botirjon@radcode.co>
2023-10-23 21:13:50 +0200

    Podcast treasure component

--- src/components/LandingPage/Home/PodcastTreasure/index.tsx
+++ src/components/LandingPage/Home/PodcastTreasure/index.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-
-import "./PodcastTreasure.css";
+import classes from "./PodcastTreasure.module.css";
 
 const PodcastTreasure = () => {
   return (
-    <div className="podcast-treasure">
-      <div className="main-container">
-        <div className="main-container-content">
-          <p className="unlock-treasure">Unlock Podcast Treasures</p>
-          <p className="unlock-tresure-content">
+    <div className={classes.podcast_treasure}>
+      <div className={classes.main_container}>
+        <div className={classes.main_container_content}>
+          <p className={classes.unlock_treasure}>Unlock Podcast Treasures</p>
+          <p className={classes.unlock_tresure_content}>
             Delve into a vast audio landscape as we guide you through
             captivating stories
           </p>
         </div>
         <div>
-          <button className="create-account-button">Create Account</button>
+          <button className={classes.create_account_button}>
+            Create Account
+          </button>
         </div>
       </div>
     </div>
