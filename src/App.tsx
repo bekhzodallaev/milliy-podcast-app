13bce44ebe6353aa4b16972c3518fa73c192d812
Botirjon Shokirov <botirjon@radcode.co>
2023-11-04 16:30:14 +0100

    installing react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/LandingPage/Home";
+import Welcome from "./components/LandingPage/Auth";
+import AboutUs from "./components/LandingPage/AboutUs";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/welcome" element={<Welcome />} />
+      <Route path="/aboutus" element={<AboutUs />} />
+
+      {/* TODO Later */}
+      {/* <Route path="/faq" element={<FAQ />} /> */}
+      {/* <Route path="/contact" element={<Contact />} /> */}
+    </Routes>
   );
 }
 
