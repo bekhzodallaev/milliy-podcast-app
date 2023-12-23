3140a753e6dcbfe3654e55fd169f85ea691673ea
bekhzod <icebreaker0210@mail.ru>
2023-12-23 13:13:34 +0100

    Add responsiveness for the Testimonials page

--- src/pages/LandingPage/Home/TestimonialsComponent/CardDetails.ts
+++ src/pages/LandingPage/Home/TestimonialsComponent/CardDetails.ts
@@ -8,12 +8,12 @@ export const responsive = {
     slidesToSlide: 3, // optional, default to 1.
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
+    breakpoint: { max: 1024, min: 800 },
     items: 2,
     slidesToSlide: 2, // optional, default to 1.
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
+    breakpoint: { max: 799, min: 0 },
     items: 1,
     slidesToSlide: 1, // optional, default to 1.
   },
