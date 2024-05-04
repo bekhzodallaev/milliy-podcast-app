7a3bacc8df3ce2684bdaf1c939d790bf37214bdc
bekhzod <icebreaker0210@mail.ru>
2024-05-04 10:34:14 +0200

    fix class related issue in contacts

--- src/main-app/Homepage/MainAppHome.tsx
+++ src/main-app/Homepage/MainAppHome.tsx
@@ -1,10 +1,35 @@
 import React from "react";
+import Bar from "../../assets/MainAppAssets/MainHomepage/menu-04.svg";
+import ProfilePhoto from "../../assets/MainAppAssets/MainHomepage/image 106.png";
+import NotificationLogo from "../../assets/MainAppAssets/MainHomepage/notification-03.svg";
+import SearchLogo from "../../assets/MainAppAssets/MainHomepage/search.svg";
 
+import styles from "./Homepage.module.css";
 const MainAppHome = () => {
   return (
     <div>
-      {" "}
-      <h1>Welcome to Homepage of the main App</h1>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <img src={Bar} alt="Podcast Bar" />
+          <p>milliypodcast</p>
+        </div>
+        <form className={styles.form}>
+          <button type="submit" className={styles.btn}>
+            {/* <SearchLogo /> */}
+            <img src={SearchLogo} alt="Search Logo" />
+          </button>
+          <input
+            type="text"
+            placeholder="Search for podcasts"
+            className={styles.input}
+          />
+        </form>
+        <div className="profile">
+          <img src={NotificationLogo} alt="Notification logo" />
+          <img src={ProfilePhoto} alt="Profile photo" />
+        </div>
+      </header>
+      <main></main>
     </div>
   );
 };
