6f1846170a5cb3a203506bbf8cc4b9889dcbfddd
bekhzod <icebreaker0210@mail.ru>
2024-06-03 00:13:12 +0200

    Add menubar toogle for the mobile

--- src/pages/LandingPage/ReusableNavbar/ReusableNavbar.tsx
+++ src/pages/LandingPage/ReusableNavbar/ReusableNavbar.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ReusableNavbar.module.css";
 import Logo from "../../../assets/Logo/logo.png";
 import { ReactComponent as PodcastLogo } from "../../../assets/Logo/podcast-black.svg";
 import { ReactComponent as MenuVector1 } from "../../../assets/LandingPageAssets/HomePage/MenuAssets/Vector 1.svg";
 import { ReactComponent as MenuVector2 } from "../../../assets/LandingPageAssets/HomePage/MenuAssets/Vector 1.svg";
 import { ReactComponent as MenuVector3 } from "../../../assets/LandingPageAssets/HomePage/MenuAssets/Vector 3.svg";
+import { ReactComponent as CancelIcon } from "../../../assets/LandingPageAssets/HomePage/MenuAssets/Group 1.svg";
 
 import { Link } from "react-router-dom";
 // const navigate = useNavigate();
@@ -19,20 +20,52 @@ import { Link } from "react-router-dom";
 // const handleAboutUsClick = () => {
 //   navigate("/aboutus");
 // };
+
 const ReusableNavbar = () => {
   const [toggle, setToggle] = React.useState(false);
+
   return (
     <div className={styles.header_container}>
       <nav className={styles.reusable_nav}>
         <section className={styles.logo}>
-          <div className={styles.menu_bar}>
-            <MenuVector1 />
-            <MenuVector2 />
-            <MenuVector3 />
+          <div
+            className={styles.menu_bar}
+            onClick={() => setToggle((oldVal) => !oldVal)}
+          >
+            {toggle ? (
+              <CancelIcon />
+            ) : (
+              <>
+                <MenuVector1 />
+                <MenuVector2 />
+                <MenuVector3 />
+              </>
+            )}
           </div>
           <img src={Logo} alt="Podcast Logo" />
           <PodcastLogo className={styles.podcast_svg} />
         </section>
+        {toggle && (
+          <div className={styles.mobile_links}>
+            <ul>
+              <li>
+                <Link to="/aboutus" className={styles.link}>
+                  ABOUT
+                </Link>
+              </li>
+              <li>
+                <Link to="/#faq" className={styles.link}>
+                  FAQ
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" className={styles.link}>
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
         <ul className={styles.links}>
           <li>
             <Link to="/aboutus" className={styles.links}>
